b32c9f8cc14767022a8154dddbe11a97e9362b80
Francisco Jose <lopezozunafranciscojose17@gmail.com>
2022-11-22 08:08:13 +0800

    Adding typing effect

---
+++ main.js
@@ -0,0 +1,11 @@
+import Typed from "typed.js";
+
+var options = {
+  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+  typeSpeed: 40
+};
+alert("hello")
+
+let h1 = document.getElementById("header");
+
+var typed = new Typed(h1, options);
